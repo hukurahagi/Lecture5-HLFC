f593c29697b06b30941e40014250f84c370f56e3
hukurahagi <hukurahagi1109@gmail.com>
2019-01-08 15:21:55 +0900

    first commit

---
+++ scripts/install-prereq.sh
@@ -0,0 +1,18 @@
+# Install
+#sudo apt-get update
+
+echo "========================================"
+echo "Starting installation - Pre requisites"
+echo "========================================"
+
+curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
+chmod u+x prereqs-ubuntu.sh
+./prereqs-ubuntu.sh
+
+echo "========================================"
+echo "Update Docker Configuration"
+echo "========================================"
+
+# December 2nd 2018
+# remove the latest version 18.09.* as it has issues 
+sudo apt-get remove -y docker-ce
